1c5ceec3c44e094c0f4187973ef483a59df26b9e
Sunny <sunny11@mail.com>
2026-09-19 17:01:26 +0000

    secrets added in repo

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express= require('express')
 const app= express()
 const port =  3000
 app.get("/" ,(req , res)=>{
-    res.send("docker build push cicd pipeline")
+    res.send("docker build push cicd pipeline buildpush2")
 } )
 
 app.listen(port , ()=>{
